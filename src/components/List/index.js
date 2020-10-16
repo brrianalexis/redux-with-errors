3d0bcee2a4c96cdf0177b3cfd63a062dce513572
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-16 19:59:56 -0300

    refactor: optional chaining just because

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Task from "../Task";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Task from '../Task';
 
 function List() {
-  const tasks = useSelector((state) => state.tasks);
+  const tasks = useSelector(state => state.tasks);
   return (
     <ul>
-      {tasks &&
-        tasks.map((value, key) => {
-          return <Task key={key} value={value} />;
-        })}
+      {tasks?.map((value, key) => {
+        return <Task key={key} value={value} />;
+      })}
     </ul>
   );
 }
